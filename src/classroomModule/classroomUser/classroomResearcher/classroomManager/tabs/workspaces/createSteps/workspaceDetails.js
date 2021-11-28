ddebbfa913d27da316c37ad68ccddb836b2be632
Thania Sinogaya <thania.sinogaya@gmail.com>
2021-11-28 17:30:49 +0800

    integration of validation is done

--- src/classroomModule/classroomUser/classroomResearcher/classroomManager/tabs/workspaces/createSteps/workspaceDetails.js
+++ src/classroomModule/classroomUser/classroomResearcher/classroomManager/tabs/workspaces/createSteps/workspaceDetails.js
@@ -1,5 +1,15 @@
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { createWorkspace } from "../../../../../../../store/workspaceSlice";
+import FeedBackButton from "../../../../../../../hooks/feedBackButton";
+import useStatus from "../../../../../../../hooks/useStatus";
+
+//mui
+import { styled } from "@mui/material/styles";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import LoadingButton from "@mui/lab/LoadingButton";
 import {
 	Button,
 	Card,
@@ -9,28 +19,28 @@ import {
 	MenuItem,
 	Select,
 	TextField,
-} from '@mui/material';
-import FeedBackButton from '../../../../../../../hooks/feedBackButton';
-import { styled } from '@mui/material/styles';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import useStatus from '../../../../../../../hooks/useStatus';
-import { useParams } from 'react-router-dom';
-import { createWorkspace } from '../../../../../../../store/workspaceSlice';
-const Input = styled('input')({
-	display: 'none',
+	Typography,
+} from "@mui/material";
+
+//validation
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+const Input = styled("input")({
+	display: "none",
 });
 
 const WorkspaceDetail = () => {
 	const { id } = useParams();
 	const defaultImage =
-		'https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
+		"https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
 	const dispatch = useDispatch();
 	const [inputForm, setInputForm] = useState({
-		name: '',
-		subject: '',
-		privacy: 'public',
-		description: '',
+		name: "",
+		subject: "",
+		privacy: "public",
+		description: "",
 		cover: defaultImage,
 		coverFile: defaultImage,
 	});
@@ -38,22 +48,22 @@ const WorkspaceDetail = () => {
 	const { status } = useSelector((state) => state.class);
 	const { loading } = useStatus(status);
 	const handleClassroomDetail = () => {
-		console.log('hello');
+		console.log("hello");
 		let form_data = new FormData();
 		const { name, description, privacy, subject, cover, coverFile } = inputForm;
 		if (coverFile != defaultImage) {
-			form_data.append('cover', coverFile, coverFile.name);
+			form_data.append("cover", coverFile, coverFile.name);
 		}
-		form_data.append('name', name);
-		form_data.append('description', description);
-		form_data.append('privacy', privacy);
-		form_data.append('subject', subject);
+		form_data.append("name", name);
+		form_data.append("description", description);
+		form_data.append("privacy", privacy);
+		form_data.append("subject", subject);
 		dispatch(createWorkspace(id, form_data));
 	};
 	console.log(inputForm.coverFile);
 	const onChange = (e) => {
 		e.preventDefault();
-		if (e.target.name == 'cover') {
+		if (e.target.name == "cover") {
 			let reader = new FileReader();
 			let file = e.target.files[0];
 
@@ -69,86 +79,116 @@ const WorkspaceDetail = () => {
 			setInputForm({ ...inputForm, [e.target.name]: e.target.value });
 		}
 	};
+
+	//validation
+	const validationMsg = Yup.object().shape({
+		name: Yup.string().required("Workspace name is required."),
+	});
+
+	const {
+		register, // register inputs
+		handleSubmit, // handle form submit
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(validationMsg),
+	});
+
+	const onSubmit = (data) => {
+		console.log(JSON.stringify(data, null, 2));
+	};
+
 	return (
-		// <div className='grid grid-cols-2 gap-4 mt-4'>
-		<div className='flex w-full justify-center'>
-			<div className='flex flex-col space-y-3 w-3/5 '>
-				<Card sx={{ maxWidth: '100%' }}>
-					<CardMedia
-						component='div'
-						image={inputForm.cover}
-						className='flex justify-end items-center'
-						sx={{
-							height: '190px',
-							display: 'flex',
-							justifyContent: 'flex-end',
-							alignItems: 'end',
-						}}
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div className="flex w-full justify-center">
+				<div className="flex flex-col space-y-3 w-3/5 ">
+					<Card sx={{ maxWidth: "100%" }}>
+						<CardMedia
+							component="div"
+							image={inputForm.cover}
+							className="flex justify-end items-center"
+							sx={{
+								height: "190px",
+								display: "flex",
+								justifyContent: "flex-end",
+								alignItems: "end",
+							}}
+						>
+							<label htmlFor="contained-button-file">
+								<Input
+									accept="image/*"
+									id="contained-button-file"
+									name="cover"
+									onChange={onChange}
+									type="file"
+								/>
+								<Button
+									variant="contained"
+									startIcon={<PhotoCamera />}
+									style={{
+										marginRight: "10px",
+										marginBottom: "10px",
+										backgroundColor: "white",
+										color: "rgba(55, 65, 81, 1)",
+										textTransform: "capitalize",
+									}}
+									component="span"
+								>
+									Change Cover Photo
+								</Button>
+							</label>
+						</CardMedia>
+					</Card>
+
+					<TextField
+						label="Workspace Name"
+						variant="outlined"
+						name="name"
+						// value={inputForm.name}
+						// onChange={(e) => onChange(e)}
+						{...register("name")}
+						error={errors.name ? true : false}
+					/>
+					<Typography
+						sx={{ fontSize: "12px", color: "red", fontStyle: "italic" }}
 					>
-						<label htmlFor='contained-button-file'>
-							<Input
-								accept='image/*'
-								id='contained-button-file'
-								name='cover'
-								onChange={onChange}
-								type='file'
-							/>
-							<Button
-								variant='contained'
-								startIcon={<PhotoCamera />}
-								style={{
-									marginRight: '10px',
-									marginBottom: '10px',
-									backgroundColor: 'white',
-									color: 'rgba(55, 65, 81, 1)',
-									textTransform: 'capitalize',
-								}}
-								component='span'
-							>
-								Change Cover Photo
-							</Button>
-						</label>
-					</CardMedia>
-				</Card>
+						{errors.name?.message}
+					</Typography>
 
-				<TextField
-					label='Workspace Name'
-					variant='outlined'
-					name='name'
-					value={inputForm.name}
-					onChange={(e) => onChange(e)}
-				/>
+					<FormControl fullWidth>
+						<InputLabel id="demo-simple-select-label">Privacy</InputLabel>
+						<Select
+							labelId="demo-simple-select-label"
+							id="demo-simple-select"
+							value={inputForm.privacy}
+							label="Privacy"
+							name="privacy"
+							onChange={(e) => onChange(e)}
+						>
+							<MenuItem value={"public"}>Public</MenuItem>
+							<MenuItem value={"private"}>Private</MenuItem>
+						</Select>
+					</FormControl>
 
-				<FormControl fullWidth>
-					<InputLabel id='demo-simple-select-label'>Privacy</InputLabel>
-					<Select
-						labelId='demo-simple-select-label'
-						id='demo-simple-select'
-						value={inputForm.privacy}
-						label='Privacy'
-						name='privacy'
+					<TextField
+						label="Description"
+						variant="outlined"
+						name="description"
+						value={inputForm.description}
 						onChange={(e) => onChange(e)}
-					>
-						<MenuItem value={'public'}>Public</MenuItem>
-						<MenuItem value={'private'}>Private</MenuItem>
-					</Select>
-				</FormControl>
+						multiline
+						minRows={4}
+					/>
 
-				<TextField
-					label='Description'
-					variant='outlined'
-					name='description'
-					value={inputForm.description}
-					onChange={(e) => onChange(e)}
-					multiline
-					minRows={4}
-				/>
-
-				<div className='flex justify-end'>
-					<Button variant='contained' onClick={handleClassroomDetail}>
+					<div className="flex justify-end">
+						{/* <Button variant="contained" onClick={handleClassroomDetail}>
 						Create Classroom
-					</Button>
-					{/* <FeedBackButton
+					</Button> */}
+
+						<Button type="submit" variant="contained">
+							Create Workspace
+						</Button>
+
+						{/* <FeedBackButton
 						button={
 							<LoadingButton
 								onClick={handleClassroomDetail}
@@ -161,10 +201,10 @@ const WorkspaceDetail = () => {
 						}
 						status={status}
 					/> */}
+					</div>
 				</div>
 			</div>
-		</div>
-		// </div>
+		</form>
 	);
 };
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,24 +1,35 @@
-import { useState, useEffect } from 'react';
-import { Redirect, Link, useHistory } from 'react-router-dom';
-import { socialLoginFacebook, login, socialLoginGoogle } from '../store/authSlice';
-import GoogleLogin from 'react-google-login';
-// import FacebookLogin from 'react-facebook-login';
-import { useDispatch, useSelector } from 'react-redux';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
-import purple from '../assets/img/Artboard 1.png';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+import { useState, useEffect } from "react";
+import { Redirect, Link, useHistory } from "react-router-dom";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import LoadingButton from '@mui/lab/LoadingButton';
-import FeedBackButton from '../hooks/feedBackButton';
-import useStatus from '../hooks/useStatus';
+import {
+	socialLoginFacebook,
+	login,
+	socialLoginGoogle,
+} from "../store/authSlice";
+// import FacebookLogin from 'react-facebook-login';
+import purple from "../assets/img/Artboard 1.png";
+
+import FeedBackButton from "../hooks/feedBackButton";
+import useStatus from "../hooks/useStatus";
+
+//mui
+import LoadingButton from "@mui/lab/LoadingButton";
+import { TextField, Box, Typography, Button } from "@mui/material";
+
+//validation
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 
 const Login = () => {
 	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
+		email: "",
+		password: "",
 	});
 	const dispatch = useDispatch();
 
@@ -29,7 +40,7 @@ const Login = () => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmitLogin = (e) => {
 		e.preventDefault();
 
 		dispatch(login(email, password));
@@ -49,21 +60,41 @@ const Login = () => {
 	const { status } = useSelector((state) => state.auth);
 	const { loading } = useStatus(status);
 	const handleRedirect = () => {
-		history.push('/home');
+		history.push("/home");
+	};
+
+	//validation
+	const validationMsg = Yup.object().shape({
+		email: Yup.string()
+			.required("Your e-mail is required.")
+			.email("Please put the correct e-mail format"),
+		password: Yup.string().required("Password is required."),
+	});
+
+	const {
+		register, // register inputs
+		handleSubmit, // handle form submit
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(validationMsg),
+	});
+
+	const onSubmit = (data) => {
+		console.log(JSON.stringify(data, null, 2));
 	};
 
 	return (
 		<>
-			<div class='flex flex-wrap w-full'>
-				<div class='flex flex-col w-full md:w-1/2'>
-					<div class='flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24'>
-						<Link to='/' class='p-4 text-xl font-bold text-white bg-purple-800'>
+			<div class="flex flex-wrap w-full">
+				<div class="flex flex-col w-full md:w-1/2">
+					<div class="flex justify-center pt-12 md:justify-start md:pl-12 md:-mb-24">
+						<Link to="/" class="p-4 text-xl font-bold text-white bg-purple-800">
 							meegu.
 						</Link>
 					</div>
-					<div class='flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32'>
-						<p class='text-3xl text-center'>Welcome back!</p>
-						<Box
+					<div class="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
+						<p class="text-3xl text-center">Welcome back!</p>
+						{/* <Box
 							component='form'
 							sx={{
 								'& > :not(style)': { m: 1, width: '500px' },
@@ -71,67 +102,91 @@ const Login = () => {
 							noValidate
 							autoComplete='off'
 							className='flex flex-col pt-3 md:pt-8 space-y-2 w-full'
+						> */}
+						<form
+							className="flex flex-col pt-3 md:pt-8 space-y-2 w-full"
+							onSubmit={handleSubmit(onSubmit)}
 						>
 							<TextField
-								variant='outlined'
+								variant="outlined"
 								fullWidth
-								name='email'
-								placeholder='Your email'
-								value={email}
-								onChange={(e) => onChange(e)}
-								type='email'
+								name="email"
+								placeholder="Your email"
+								type="email"
+								// value={email}
+								// onChange={(e) => onChange(e)}
+								{...register("email")}
+								error={errors.email ? true : false}
 							/>
-							<TextField
-								variant='outlined'
-								fullWidth
-								name='password'
-								placeholder='Your password'
-								value={password}
-								onChange={(e) => onChange(e)}
-								type='password'
-							/>
+							<Typography
+								sx={{ fontSize: "12px", color: "red", fontStyle: "italic" }}
+							>
+								{errors.email?.message}
+							</Typography>
 
-							<div className='flex flex-row justify-between'>
-								<div class='pt-12 pb-12 text-center justify-start'>
+							<TextField
+								variant="outlined"
+								fullWidth
+								name="password"
+								placeholder="Your password"
+								type="password"
+								// value={password}
+								// onChange={(e) => onChange(e)}
+								{...register("password")}
+								error={errors.password ? true : false}
+							/>
+							<Typography
+								sx={{ fontSize: "12px", color: "red", fontStyle: "italic" }}
+							>
+								{errors.password?.message}
+							</Typography>
+
+							<div className="flex flex-row justify-between">
+								<div class="pt-12 pb-12 text-center justify-start">
 									<p>
 										<Link
-											to='/signup'
-											className='text-base ml-2 text-gray-400 hover:text-purple-400'
+											to="/signup"
+											className="text-base ml-2 text-gray-400 hover:text-purple-400"
 										>
 											Register
 										</Link>
 									</p>
 								</div>
 
-								<div class='pt-12 pb-12 text-center justify-start'>
+								<div class="pt-12 pb-12 text-center justify-start">
 									<p>
 										<Link
-											to='/reset-password'
-											class='-ml-8 text-gray-400 hover:text-purple-400'
+											to="/reset-password"
+											class="-ml-8 text-gray-400 hover:text-purple-400"
 										>
 											Forgot your password?
 										</Link>
 									</p>
 								</div>
 
-								<div className='mt-9 justify-between'>
-									<FeedBackButton
+								<div className="mt-9 justify-between">
+									{/* <FeedBackButton
 										button={
 											<LoadingButton
-												onClick={handleSubmit}
+												onClick={handleSubmitLogin}
 												loading={loading}
-												type='submit'
-												loadingPosition='end'
-												variant='contained'
-												className='w-36 px-4  py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-purple-800 shadow-md hover:text-purple-800 hover:bg-white focus:outline-none focus:ring-2 rounded-md'
+												type="submit"
+												loadingPosition="end"
+												variant="contained"
+												className="w-36 px-4  py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-purple-800 shadow-md hover:text-purple-800 hover:bg-white focus:outline-none focus:ring-2 rounded-md"
 											>
 												Login
 											</LoadingButton>
 										}
 										status={status}
 										actionSuccess={handleRedirect}
-										actionDescription='Login'
-									/>
+										actionDescription="Login"
+									/> */}
+									<div>
+										<Button type="submit" variant="contained">
+											Login
+										</Button>
+									</div>
 
 									{/* <button
 										onClick={(e) => handleSubmit(e)}
@@ -142,53 +197,53 @@ const Login = () => {
 									</button> */}
 								</div>
 							</div>
-							<div className='flex flex-row justify-between mb-12 px-10'>
-								<div className='text-gray-500'>Continue with: </div>
-								<div class='flex flex-row space-x-6 pr-10'>
-									<div>
-										<FacebookLogin
-											appId='564451954655803'
-											autoLoad={false}
-											fields='name,email,picture'
-											callback={responseFacebook}
-											render={(renderProps) => (
-												<button
-													onClick={renderProps.onClick}
-													className='text-purple-400'
-												>
-													Facebook
-												</button>
-											)}
-										/>
-									</div>
-									<div>
-										<GoogleLogin
-											clientId='1090422806656-3gpck8pb13jj38c9bp25pmuqe6scgsb1.apps.googleusercontent.com'
-											render={(renderProps) => (
-												<button
-													onClick={renderProps.onClick}
-													disabled={renderProps.disabled}
-													className='text-purple-400'
-												>
-													Google
-												</button>
-											)}
-											onSuccess={(e) => responseGoogle(e)}
-											onFailure={(e) => responseGoogle(e)}
-										/>
-									</div>
+						</form>
+						<div className="flex flex-row justify-between mb-12 px-10">
+							<div className="text-gray-500">Continue with: </div>
+							<div class="flex flex-row space-x-6 pr-10">
+								<div>
+									<FacebookLogin
+										appId="564451954655803"
+										autoLoad={false}
+										fields="name,email,picture"
+										callback={responseFacebook}
+										render={(renderProps) => (
+											<button
+												onClick={renderProps.onClick}
+												className="text-purple-400"
+											>
+												Facebook
+											</button>
+										)}
+									/>
+								</div>
+								<div>
+									<GoogleLogin
+										clientId="1090422806656-3gpck8pb13jj38c9bp25pmuqe6scgsb1.apps.googleusercontent.com"
+										render={(renderProps) => (
+											<button
+												onClick={renderProps.onClick}
+												disabled={renderProps.disabled}
+												className="text-purple-400"
+											>
+												Google
+											</button>
+										)}
+										onSuccess={(e) => responseGoogle(e)}
+										onFailure={(e) => responseGoogle(e)}
+									/>
 								</div>
 							</div>
-						</Box>
+						</div>
 						{/* <form class='flex flex-col pt-3 md:pt-8 space-y-2'>
 						</form> */}
 					</div>
 				</div>
-				<div class='w-1/2 shadow-2xl'>
+				<div class="w-1/2 shadow-2xl">
 					<img
-						class='hidden object-center w-full h-screen md:block'
+						class="hidden object-center w-full h-screen md:block"
 						src={purple}
-						alt='login'
+						alt="login"
 					/>
 				</div>
 			</div>
