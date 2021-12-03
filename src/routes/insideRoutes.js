16a76c2bc0103809c338149626c02e555afaacf0
Thania Sinogaya <thania.sinogaya@gmail.com>
2021-12-03 16:54:24 +0800

    integrating design is done

--- src/routes/insideRoutes.js
+++ src/routes/insideRoutes.js
@@ -1,12 +1,14 @@
 import { Route } from "react-router";
 
-import NewMesssages from "../materialUI/pages/messages/NewMesssages";
+import NewMessages from "../materialUI/pages/messages/NewMesssages";
+import Messages from "../messageModule/messages";
 import Home from "../pages/home";
 import Logout from "../pages/logout";
 import Profile from "../pages/profile";
 
 export default [
-	<Route exact path="/messages" component={NewMesssages} />,
+	<Route exact path="/messages" component={Messages} />,
+	<Route exact path="/prev-messages" component={NewMessages} />,
 	<Route path="/profile" component={Profile} />,
 	<Route path="/home" component={Home} />,
 	<Route path="/logout" component={Logout} />,
